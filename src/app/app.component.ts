33a13793b53548a68c3c48ce0b083fc785495c15
Tauhidul Alam <tauhid@Tauhiduls-MacBook-Pro.local>
2019-12-27 11:26:21 +0600

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder, AbstractControl } from '@angular/forms';
+import { parsePhoneNumberFromString, ParseError, AsYouType } from 'libphonenumber-js';
+
+import { AppService } from './app.component.service';
+import { ICountry } from './app.component.interface';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  selectedCountry: any = 'US';
+  selectedPhoneNumber: any;
+  countries: any[];
+	register: FormGroup;
+
+  constructor(private fb: FormBuilder, private appService: AppService){}
+
+  ngOnInit(){
+    this.fetchCountryList();
+    this.initForm();
+  }
+
+  private fetchCountryList(){
+    this.appService.getCountries().subscribe((res: ICountry[]) => {
+			this.countries = res;
+		}, error => error);
+  }
+
+  private initForm(){
+    this.register = this.fb.group({
+			phone: ['', [Validators.required, this._validatePhoneNumberInput.bind(this)]]
+		});
+  }
+
+  _validatePhoneNumberInput(c: AbstractControl){
+    let inputValue: string = c.value.toString();
+    let phoneNumber: any = parsePhoneNumberFromString(inputValue, this.selectedCountry);
+    if(phoneNumber){
+      if(phoneNumber.isValid()){
+        return null;
+      } else {
+        return {
+          phoneNumber: {
+            valid: false
+          }
+        }
+      }
+    } else {
+      return {
+        phoneNumber: {
+          valid: false
+        }
+      }
+    }
+	}
+
+
+  resetPhoneNumber(event){
+		this.register.controls['phone'].setValue('');
+	}
+
+  formatPhoneNumber(event){
+		let inputValue: any = this.register.controls['phone'].value;
+		let phoneNumber: any = parsePhoneNumberFromString(inputValue, this.selectedCountry);
+		if(phoneNumber){
+			this.selectedPhoneNumber = phoneNumber.number;
+			this.register.controls['phone'].setValue(phoneNumber.formatInternational());
+		}
+	}
+}
